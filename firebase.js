1d6141ea7d26ab24ae2783b273302de7f4e7205d
Rida2417 <154396591+Rida2417@users.noreply.github.com>
2024-08-20 14:07:50 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -8,13 +8,13 @@ import { getFirestore} from 'firebase/firestore'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAUatAi0upgwalV2ac4y_VdPuzwatL9K1M",
-  authDomain: "flashcardsproject-926d8.firebaseapp.com",
-  projectId: "flashcardsproject-926d8",
-  storageBucket: "flashcardsproject-926d8.appspot.com",
-  messagingSenderId: "503661618380",
-  appId: "1:503661618380:web:97e5333f87f6cfe0a50e69",
-  measurementId: "G-QWFX3GGEVN"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
